a943938b6d23cdddf608759dbe51b2491e405ad4
Intisar Ahmed Siyan <intisar.siyan5550@gmail.com>
2024-01-10 14:24:57 +0600

    Module 10.5 finished

--- Module 10.5/sample1/first_app/views.py
+++ Module 10.5/sample1/first_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+import datetime
 # Create your views here.
+
+def home(request):
+    d= {'author': 'Rahim','age':20,'lst':['python','is','best'],'today':datetime.datetime.now(), 'lst2' : [
+    {'name': 'Josh', 'age': 19},
+    {'name': 'Dave', 'age': 22},
+    {'name': 'Joe', 'age': 31},
+    ],}
+
+    return render(request,'first_app/home.html',d)
