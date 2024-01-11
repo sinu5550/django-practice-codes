65a6804c46ea0e7737e834d8b40a747c4c8f6394
Intisar Ahmed Siyan <intisar.siyan5550@gmail.com>
2024-01-12 03:51:31 +0600

    Models Field added done

--- Module 14.5/module_14_5/first_app/models.py
+++ Module 14.5/module_14_5/first_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class MyModel(models.Model):
+        name = models.CharField(max_length=20)
+        roll = models.AutoField(primary_key=True)
+        address = models.TextField()
+        boolean_field = models.BooleanField(default=True)
+        
+        def __str__(self):
+                return  f"Roll: {self.roll} - Name: {self.name}"
